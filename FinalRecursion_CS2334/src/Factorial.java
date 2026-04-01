e1bd0e5bc1e0f6e897f4db7bbf5cbc72ed806731
CameronM63 <ch47cam@gmail.com>
2026-04-01 13:07:40 -0500

    Update Factorial.java

--- FinalRecursion_CS2334/src/Factorial.java
+++ FinalRecursion_CS2334/src/Factorial.java
@@ -1,4 +1,97 @@
+import java.math.BigInteger;
 
-public class factorial {
+public class Factorial {
+	public static BigInteger recFactorial(int number) {
+		BigInteger result = BigInteger.valueOf(number);
+		if (number <= 1) {
+			return BigInteger.ONE; // base case
+		}
+		else {
+			result = result.multiply(recFactorial(number - 1)); // recursive case
+			return result;
+		}
+	}
 
+	public static BigInteger itrFactorial(int number) {
+		BigInteger result = BigInteger.ONE;
+		for(int i = 2; i <= number; ++i) {
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int testData1 = 5000; // 5k
+		int testData2 = 10000; // 10k
+		int testData3 = 30000; // 5k
+		int testData4 = 50000; // 5k
+		int testData5 = 70000; // 5k
+		int testData6 = 100000; // 5k
+		long startTime = 0;
+		long elapsedNanos = 0;
+		long totalTime = 0;
+		BigInteger result = BigInteger.ZERO;
+		int[] testSizes = {testData1, testData2, testData3, testData4, testData5, testData6};
+
+		
+		/*RECURSION TESTING CODE*/
+		
+		// Warming up Java for recursion
+		for(int i = 0; i < 5000; i++) { 
+			recFactorial(testData1); 
+		}
+
+		System.out.println("RECURSION");
+		for (int size : testSizes) {
+			// Average runtime calculation (10 iterations)
+			try {
+				for(int i = 0; i < 10; i++) {
+					System.gc();
+					startTime = System.nanoTime();
+					// ... the code being measured ...
+					result = recFactorial(testData1); // storing the result so that it does not get skipped in time calculation
+					elapsedNanos = System.nanoTime() - startTime; // Calculate time spent on operation
+					totalTime = totalTime + elapsedNanos; // Total time spent for the iterations
+				}
+				// Total time spent divided by the amount of iterations
+				System.out.println("Recursive Average for " + size + ": " + (totalTime / 10));
+			}
+			catch (StackOverflowError e) {
+				System.out.println("Recursion Cliff reached at: " + size);
+				break; // Stop testing recursion once it fails
+			}
+		}
+
+		
+		/*ITERATION TESTING CODE*/
+
+		// Warming up Java for iteration
+		for(int i = 0; i < 5000; i++) { 
+			itrFactorial(testData1); 
+		}
+
+		System.out.println("ITERATION");
+		for (int size : testSizes) {
+			// Average runtime calculation (10 iterations)
+			try {
+				for(int i = 0; i < 10; i++) {
+					System.gc();
+					startTime = System.nanoTime();
+					// ... the code being measured ...
+					result = itrFactorial(testData1); // storing the result so that it does not get skipped in time calculation
+					elapsedNanos = System.nanoTime() - startTime; // Calculate time spent on operation
+					totalTime = totalTime + elapsedNanos; // Total time spent for the iterations
+				}
+				// Total time spent divided by the amount of iterations
+				System.out.println("Iteration Average for " + size + ": " + (totalTime / 10)); 
+			}
+			catch (StackOverflowError e) {
+				System.out.println("Iteration Cliff reached at: " + size);
+				break; // Stop testing iteration once it fails
+			}
+		}
+
+
+	}
 }
