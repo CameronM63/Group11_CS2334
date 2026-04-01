9d04efa0910d099c7ff81c595ff41724a72da257
Nyeloz <149612042+Nyeloz@users.noreply.github.com>
2026-03-31 22:20:00 -0500

    Implementing the recursive and iterative methods
    implemented and tested on a small set of numbers, as well as refactoring
    classnames to proper coventions

--- FinalRecursion_CS2334/src/Binary.java
+++ FinalRecursion_CS2334/src/Binary.java
@@ -1,55 +1,39 @@
 
 public class Binary {
-	class Geeks{
-	    
-	    static int binarySearch(int a[], int l, int r, int x){
-	        
-	        while (l <= r){
-	            
-	            int m = (l + r) / 2;
-
-	            // Index of Element Returned
-	            if (a[m] == x) {
-	                return m;
-
-	                // If element is smaller than mid, then
-	                // it can only be present in left subarray
-	                // so we decrease our r pointer to mid - 1
-	            }
-	            else if (a[m] > x) {
-	                r = m - 1;
-
-	                // Else the element can only be present
-	                // in right subarray
-	                // so we increase our l pointer to mid + 1
-	            }
-	            else {
-	                l = m + 1;
-	            }
-	        }
-
-	        // No Element Found
-	        return -1;
-	    }
-
-	    public static void main(String args[])
-	    {
-
-	        int a[] = { 2, 3, 4, 10, 40 };
-	        int n = a.length;
-	        int x = 10;
-
-	        int res = binarySearch(a, 0, n - 1, x);
-
-	        System.out.println("Element to be searched is : "
-	                           + x);
-
-	        if (res == -1)
-	            System.out.println(
-	                "Element is not present in array");
-	        else
-	            System.out.println(
-	                "Element is present at index: " + res);
-	    }
+	
+	//finds index
+	public int iterationBinarySearch(int[] arr, int target, int start, int end) {
+		
+		int midVal = 0;
+		while(start <= end) {
+			midVal = start + ( (end - start) / 2);
+			
+			if(arr[midVal]  < target) {
+				start = midVal + 1;
+			}else if(arr[midVal] > target) {
+				end = midVal - 1;
+			}else if(arr[midVal] == target) {
+				return midVal;
+			}
+		}
+		return  -1;
+	
+	}
+	
+	public int recursiveBinarySearch(int[] arr, int target, int start, int end) {
+		
+		int midVal = start + ( (end - start) / 2);
+		
+		if(end < start) {
+			return - 1;
+		}
+		
+		if(target == arr[midVal]) {
+			return midVal;
+		}else if(target < arr[midVal]) {
+			return recursiveBinarySearch(arr, target, start, midVal - 1);
+		}else {
+			return recursiveBinarySearch(arr, target, midVal + 1, end);
+		}
 	}
 }
--- FinalRecursion_CS2334/src/Driver.java
+++ FinalRecursion_CS2334/src/Driver.java
@@ -3,7 +3,11 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		int[] practice = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13, 14, 15, 16,17,18,19,20};
+		int indexSize = practice.length  - 1;;
+		Binary maliaMethod = new Binary();
+		System.out.println(maliaMethod.iterationBinarySearch(practice, 14, 2, 17));
+		System.out.println(maliaMethod.recursiveBinarySearch(practice, 14, 2, 17));
 	}
 
 }
